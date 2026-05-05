a197d781fa4ad20d6b632c6acb1821bc86d64b2e
Greensky <144330872+sriManikanta07@users.noreply.github.com>
2026-05-05 16:30:07 +0530

    Update socket.js

--- client/chat_app_frontend/src/socket.js
+++ client/chat_app_frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chat-app-backend-h1ex.onrender.com");
 
 socket.on("connect", () => {
   console.log("Socket connected:", socket.id);
